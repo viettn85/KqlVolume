69aa02da554b8625a464197f5ae6f803916942c3
Viet Tran <viettn85@gmail.com>
2021-08-16 23:32:05 +0700

    Skip errors

--- src/chart/ducky.py
+++ src/chart/ducky.py
@@ -13,55 +13,58 @@ data_location = os.getenv("data")
 data_realtime = data_location + os.getenv("data_realtime")
 
 def draw(stock, location):
-    df = pd.read_csv("{}{}.csv".format(data_realtime, stock), parse_dates=True)
-    getIndicators(df)
-    df.index = pd.DatetimeIndex(df['Date'])
-    df['ADX20'] = 20
-    df['RSI70'] = 70
-    df['RSI30'] = 30
-    df['Stoch20'] = 20
-    df['Stoch80'] = 80
-    df['MACD0'] = 0
-    df = df[0:150]
-    df.sort_index(ascending=True, inplace=True)
-    
-    
-    mc = fplt.make_marketcolors(
-                            up='tab:blue',down='tab:red',
-                            volume='inherit',
-    )
-    apds = [ fplt.make_addplot(df['MA20'], panel=0,color='red'),
-                fplt.make_addplot(df['MA50'], panel=0,color='blue'),
-                fplt.make_addplot(df['MA200'], panel=0,color='green'),
-                fplt.make_addplot(df['Volume'], type = 'line', linestyle=' ', panel =1, mav = 20, color='g'),
-                fplt.make_addplot(df['MACD'], panel=2,color='blue'),
-                fplt.make_addplot(df['MACD_SIGNAL'], panel=2,color='red'),
-                fplt.make_addplot(df['MACD0'], panel=2,color='grey'),
-                fplt.make_addplot(df['%K'], panel=3, color='red'),
-                fplt.make_addplot(df['%D'], panel=3,color='blue'),
-                fplt.make_addplot(df['Stoch20'], panel=3,color='grey'),
-                fplt.make_addplot(df['Stoch80'], panel=3,color='grey'),
-                fplt.make_addplot(df['RSI'], panel=4,color='red'),
-                fplt.make_addplot(df['RSI70'], panel=4,color='grey'),
-                fplt.make_addplot(df['RSI30'], panel=4,color='grey'),
-                fplt.make_addplot(df['ADX'], panel=5,color='blue'),
-                fplt.make_addplot(df['PDI'], panel=5,color='green'),
-                fplt.make_addplot(df['NDI'], panel=5,color='red'),
-                fplt.make_addplot(df.ADX20, type = 'line', panel=5,color='grey')
-                ]
-    # s  = fplt.make_mpf_style(base_mpl_style="seaborn", y_on_right=True, marketcolors=mc, mavcolors=["red","orange","skyblue"])
-    s  = fplt.make_mpf_style(base_mpl_style="seaborn", y_on_right=True, marketcolors=mc)
-    fplt.plot(
-                df,
-                type='candle',
-                style=s,
-                title=stock,
-                ylabel='',
-                # mav=(20, 50, 200),
-                volume=True,
-                addplot=apds,
-                savefig=dict(fname='{}/{}.png'.format(location, stock),dpi=100,pad_inches=0.25)
-    )
+    try:
+        df = pd.read_csv("{}{}.csv".format(data_realtime, stock), parse_dates=True)
+        getIndicators(df)
+        df.index = pd.DatetimeIndex(df['Date'])
+        df['ADX20'] = 20
+        df['RSI70'] = 70
+        df['RSI30'] = 30
+        df['Stoch20'] = 20
+        df['Stoch80'] = 80
+        df['MACD0'] = 0
+        df = df[0:150]
+        df.sort_index(ascending=True, inplace=True)
+        
+        
+        mc = fplt.make_marketcolors(
+                                up='tab:blue',down='tab:red',
+                                volume='inherit',
+        )
+        apds = [ fplt.make_addplot(df['MA20'], panel=0,color='red'),
+                    fplt.make_addplot(df['MA50'], panel=0,color='blue'),
+                    fplt.make_addplot(df['MA200'], panel=0,color='green'),
+                    fplt.make_addplot(df['Volume'], type = 'line', linestyle=' ', panel =1, mav = 20, color='g'),
+                    fplt.make_addplot(df['MACD'], panel=2,color='blue'),
+                    fplt.make_addplot(df['MACD_SIGNAL'], panel=2,color='red'),
+                    fplt.make_addplot(df['MACD0'], panel=2,color='grey'),
+                    fplt.make_addplot(df['%K'], panel=3, color='red'),
+                    fplt.make_addplot(df['%D'], panel=3,color='blue'),
+                    fplt.make_addplot(df['Stoch20'], panel=3,color='grey'),
+                    fplt.make_addplot(df['Stoch80'], panel=3,color='grey'),
+                    fplt.make_addplot(df['RSI'], panel=4,color='red'),
+                    fplt.make_addplot(df['RSI70'], panel=4,color='grey'),
+                    fplt.make_addplot(df['RSI30'], panel=4,color='grey'),
+                    fplt.make_addplot(df['ADX'], panel=5,color='blue'),
+                    fplt.make_addplot(df['PDI'], panel=5,color='green'),
+                    fplt.make_addplot(df['NDI'], panel=5,color='red'),
+                    fplt.make_addplot(df.ADX20, type = 'line', panel=5,color='grey')
+                    ]
+        # s  = fplt.make_mpf_style(base_mpl_style="seaborn", y_on_right=True, marketcolors=mc, mavcolors=["red","orange","skyblue"])
+        s  = fplt.make_mpf_style(base_mpl_style="seaborn", y_on_right=True, marketcolors=mc)
+        fplt.plot(
+                    df,
+                    type='candle',
+                    style=s,
+                    title=stock,
+                    ylabel='',
+                    # mav=(20, 50, 200),
+                    volume=True,
+                    addplot=apds,
+                    savefig=dict(fname='{}/{}.png'.format(location, stock),dpi=100,pad_inches=0.25)
+        )
+    except:
+        print("Error to export {}".format(stock))
 
 import cufflinks as cf
 import chart_studio.plotly as py
